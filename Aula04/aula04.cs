ae0dfebaa14a6617309f20a1d831455b605d089e
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-10-26 17:24:50 -0300

    .

---
+++ Aula04/aula04.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula03{
+    static int num = 10;
+    static void Main(string [] args){
+        Console.WriteLine(num);
+    }
+    void teste(){
+        int num2 = 2;
+        Console.WriteLine(num2);
+    }
+}
