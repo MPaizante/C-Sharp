536d0e5bd21a18ad4ce3307e60cc03c33f1bd4f3
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-10-26 14:34:47 -0300

    .

---
+++ Aula03/aula03.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula03{
+    static void Main(string [] args){
+        int num = 0;
+        char letra = 'c';
+        float valor = 5.3f;
+        byte n1 = 10;
+        string nome = "Matheus";
+
+        var aux = nome;
+
+        Console.WriteLine("Valor da Variavel " + aux);
+    }
+}
